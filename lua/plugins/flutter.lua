990b04b76de01b7fc7bd1432830582cd88b8bfdf
0bim <pulpits_search.0f@icloud.com>
2025-05-27 18:08:52 -0400

    fix merge conflict

--- lua/plugins/flutter.lua
+++ lua/plugins/flutter.lua
@@ -1,19 +1,8 @@
 return {
-<<<<<<< HEAD
-    'akinsho/flutter-tools.nvim',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        'stevearc/dressing.nvim',
-    },
-    config = true,
-=======
-  -- lua/processfiles/init.lua
-  'nvim-flutter/flutter-tools.nvim',
-  lazy = false,
+  'akinsho/flutter-tools.nvim',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'stevearc/dressing.nvim', -- optional for vim.ui.select
+    'stevearc/dressing.nvim',
   },
   config = true,
->>>>>>> 5c76f5008fe9f8813f0817d767a32eb80719d6a1
 }
